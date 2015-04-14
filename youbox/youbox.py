eb594bc288004c6286301f7981c501b3e74dfbe7
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2015-04-14 14:08:44 +0530

    just another cm-ommit

--- youbox/youbox.py
+++ youbox/youbox.py
@@ -1,13 +1,18 @@
- 
-def  increment_count(v_id) :
+import db_connect.py
+import os.path
+
+
+DDIR = "./downloads/"
+video_types = ['mp4','ogg']
+def  increment_count(vid) :
 
  
  
-def  mark_for_download(v_id): #if not exists 
+def  mark_for_download(vid): #if not exists 
  
  
  
-def  unmark_from_download(v_id): #if exists 
+def  unmark_from_download(vid): #if exists 
  
  
  
@@ -15,57 +20,64 @@ def  sync_with_server() :
  
  
  
-def  download_from_internet(v_id) :
+def  download_from_internet(vid) :
  
 	# code for fetching download url 
 	# code for downloading 
-	# code for saving with filename v_id 
+	# code for saving with filename vid 
 	unmark_from_download() 
 	sync_with_server() 
  
-def  download_from_network(v_id , url) :
+def  download_from_network(vid , url) :
  
 	# code for downloading 
-	# code for saving with filename v_id 
+	# code for saving with filename vid 
 	unmark_from_download() 
 	sync_with_server() 
  
 def  idle_download() :
  
-	# code to pick v_id from marked ones 
+	# code to pick vid from marked ones 
 	# if not exists return 0 
 	# else 
-	download_from_internet(v_id) 
+	download_from_internet(vid) 
 	return 1 
  
-def  get_from_server(v_id) :
+def  get_from_server(vid) :
  
 	 
  
-def  get_from_local(v_id) :
+def  get_from_local(vid) :
+	for t in video_types:
+		f = DDIR+vid+"."+t
+		if os.path.isfile(f) :
+			return f
+	return ""
+ 
+
+def get_url(vid,rid):
+	local = get_from_local(vid)
+	if local == "":
+		glb = get_from_server(vid)
+		if glb == "":
+			link = download_from_internet(vid)
+			executer("update requests set dtime = now(), foundat = 'internet', link = '{0}' where rid = {1}".format(link,rid))
+			return get_from_local(vid)
+		else:
+			download_from_network(vid,glb)
+			executer("update requests set dtime = now(), foundat = 'global', link = '{0}' where rid = {1}".format(glb,rid))
+			return get_from_local(vid)
+	else:
+		executer("update requests set dtime = now(), foundat = 'local', link = '{0}' where rid = {1}".format(local,rid))
+		return local
  
  
  
- 
-def  get_url(v_id) :
- 
-	local = get_from_local(v_id) 
-	if local == "":	 
-		glb =  get_from_server(v_id) 
-		if glb == "" :
-			return "" 
-		else :		 
-			download_from_internet(v_id,glb) 
-			return get_from_local(v_id)  
-	else :
-		return local 
- 
- 
- 
-def  request(v_id) :
- 
-	increment_count(v_id) 
-	url = get_url(v_id) 
+def  request(vid, requester = "anon" ) : 
+	executer("insert into requests(vid,requester,rtime) values ('{0}', '{1}',now())".format(vid,requester))
+	executer("select last_insert_id()")
+	rid = fetcher()[0][0]
+	url = get_url(vid,rid) 
 	return url 
  
  
