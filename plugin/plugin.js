4ac3b42002d3e895521122632141ec524966174b
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2015-12-16 14:36:10 +0530

    just push

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -7,7 +7,18 @@ function get_vid()
 
 function replace_player(url)
 {
-
+	var child = document.getElementById("player-mole-container");
+    var placer = document.getElementById("placeholder-player");
+    var video = document.createElement("video");
+    video.src = url;
+    video.autoPlay = true;
+    video.controls = true;
+    video.width = "640";
+    video.height = "390";
+    var par = child.parentNode;
+    par.removeChild(child);
+    placer.parentNode.removeChild(placer);
+    par.appendChild(video);
 }
 
 function set_page()
