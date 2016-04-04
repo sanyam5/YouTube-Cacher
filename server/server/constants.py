eadf33412049f82a4690307ac3f73603f215255b
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2016-04-04 06:33:24 +0530

    make the download directory generic

--- server/server/constants.py
+++ server/server/constants.py
@@ -1,4 +1,5 @@
-DOWNLOAD_DIR = "/home/hawck/programming/fun/youtube/YouTube-Cacher/downloads"
+import os
+DOWNLOAD_DIR = os.path.dirname(os.path.abspath(__file__)) + "/../../downloads"
 RELEVANT_HISTORY = 30 #in days, the duration that should be considered when deciding what to cache
 NUM_MAX_CACHE = 30 # max number of videos that can be cached at one time.
 NUM_MUST_HAVE = 20 # = x such that top x videos must always be cached
